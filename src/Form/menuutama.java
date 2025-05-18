ac845d0e838e724d4c424e21e3fd8d1fecd3b69e
relaxris <muhammadharissyawaludin@gmail.com>
2025-05-18 15:30:50 +0700

    Revert "Delete src/Form/daftar.java"
    This reverts commit e414c2e403dd0c29c22501a61e1bd459e2f5d1e2.
    
    # Conflicts:
    #	src/Form/daftar.java

--- src/Form/menuutama.java
+++ src/Form/menuutama.java
@@ -4,13 +4,20 @@
  * and open the template in the editor.
  */
 package Form;
+import java.sql.*;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+import Koneksi.koneksi;
 
 /**
  *
  * @author user
  */
 public class menuutama extends javax.swing.JFrame {
-
+private Connection conn = new koneksi().connect();  
     /**
      * Creates new form menuutama
      */
@@ -36,6 +43,8 @@ public class menuutama extends javax.swing.JFrame {
         barang = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -83,6 +92,18 @@ public class menuutama extends javax.swing.JFrame {
         jMenu2.add(jMenuItem1);
 
         jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Laporan");
+
+        jMenuItem2.setText("Laporan Data Pelanggan");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu3);
 
         setJMenuBar(jMenuBar1);
 
@@ -133,6 +154,17 @@ public class menuutama extends javax.swing.JFrame {
         frmN.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        try {
+        String path = "./src/pelanggan.jasper"; // letak penyimpanan report
+        HashMap parameter = new HashMap();
+        JasperPrint print = JasperFillManager.fillReport(path, parameter, conn);
+        JasperViewer.viewReport(print, false);
+    } catch (Exception ex) {
+        JOptionPane.showMessageDialog(rootPane, "Dokumen Tidak Ada " + ex);
+    }// TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -174,8 +206,10 @@ public class menuutama extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem kasir;
     private javax.swing.JMenuItem pelanggan;
     // End of variables declaration//GEN-END:variables
